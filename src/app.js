d1d68af58e6519a7405b5eb362a64372fb75c32c
ramdhannassyirah <ramdhannassyirahh@gmail.com>
2024-03-07 15:40:42 +0700

    server

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use("/img", express.static(PATH.join(__dirname, "./public/img")));
 
-const db = require("../models");
+const db = require("../src/models");
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
@@ -25,8 +25,8 @@ app.get("/", (req, res) => {
   res.json({message: "Hello World"});
 });
 
-require("../routes/product.route")(app);
-require("../routes/order.route")(app);
+require("../src/routes/product.route")(app);
+require("../src/routes/order.route")(app);
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
