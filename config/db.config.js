b8eb6819af642dda94049a03ebeba4425e08f9d2
ramdhannassyirah <ramdhannassyirahh@gmail.com>
2024-03-11 13:49:28 +0700

    server

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb://mongo:gvKQMZEgvOdqXDYuJjiMyrJLhbcAHORB@roundhouse.proxy.rlwy.net:14614/collect_shopping",
+  url: process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/collect_shopping",
 };
